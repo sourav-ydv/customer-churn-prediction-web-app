0c28e3755bbc050acc5cc0885137e1a979037dc0
sourav-ydv <sk.souravrao25@gmail.com>
2025-10-27 20:27:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ Created on Mon Oct 27 12:30:00 2025
 
 @author: sksou
 """
-# -*- coding: utf-8 -*-
-"""
-Customer Churn Prediction Web App with Spaced Columns
-"""
 
 import numpy as np
 import pickle
@@ -30,9 +26,10 @@ def churn_prediction(input_data):
 def main():
     st.title("📊 Customer Churn Prediction Web App")
 
-    # Columns with spacing (ratios: col, space, col, space, col)
-    col1, space1, col2, space2, col3 = st.columns([1, 0.2, 1, 0.2, 1])
+    # Create 4 columns with spacing
+    col1, space1, col2, space2, col3, space3, col4 = st.columns([1, 0.2, 1, 0.2, 1, 0.2, 1])
 
+    # Column 1 - Personal info
     with col1:
         gender = st.selectbox("Gender", ("Female", "Male"))
         SeniorCitizen = st.selectbox("Senior Citizen", (0, 1))
@@ -40,6 +37,7 @@ def main():
         Dependents = st.selectbox("Dependents", ("Yes", "No"))
         tenure = st.number_input("Tenure (in months)", min_value=0, max_value=100, step=1)
 
+    # Column 2 - Phone & Internet
     with col2:
         PhoneService = st.selectbox("Phone Service", ("Yes", "No"))
         MultipleLines = st.selectbox("Multiple Lines", ("No phone service", "No", "Yes"))
@@ -47,12 +45,16 @@ def main():
         OnlineSecurity = st.selectbox("Online Security", ("No", "Yes", "No internet service"))
         OnlineBackup = st.selectbox("Online Backup", ("No", "Yes", "No internet service"))
 
+    # Column 3 - Services
     with col3:
         DeviceProtection = st.selectbox("Device Protection", ("No", "Yes", "No internet service"))
         TechSupport = st.selectbox("Tech Support", ("No", "Yes", "No internet service"))
         StreamingTV = st.selectbox("Streaming TV", ("No", "Yes", "No internet service"))
         StreamingMovies = st.selectbox("Streaming Movies", ("No", "Yes", "No internet service"))
         Contract = st.selectbox("Contract", ("Month-to-month", "One year", "Two year"))
+
+    # Column 4 - Billing
+    with col4:
         PaperlessBilling = st.selectbox("Paperless Billing", ("Yes", "No"))
         PaymentMethod = st.selectbox(
             "Payment Method",
